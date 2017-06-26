bbdf6af2f6017e873a890501af583d5f0ce78a5e
ian0411 <ian0411@hotmail.com>
2017-06-26 19:37:10 +0000

    Change to Lesson 36

--- control_panel/urls.py
+++ control_panel/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 
-from newsletters.views import control_newsletter
+from newsletters.views import control_newsletter, control_newsletter_list
 
 urlpatterns = [
     url(r'^newsletter/$', control_newsletter, name='control_newsletter'),
+    url(r'^newsletter-list/$', control_newsletter_list, name='control_newsletter_list'),
 ]
